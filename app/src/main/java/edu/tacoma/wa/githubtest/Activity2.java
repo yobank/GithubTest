ea0af9b838140cf47475421bbd84461b52c9a51d
TheWumboMan <57309325+TheWumboMan@users.noreply.github.com>
2020-04-12 14:50:34 -0700

    Added a comment

--- app/src/main/java/edu/tacoma/wa/githubtest/Activity2.java
+++ app/src/main/java/edu/tacoma/wa/githubtest/Activity2.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class Activity2 extends AppCompatActivity {
 
+    // Comment from James
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

c0154e91d820ce96875783d677630bcc7d0ddd23
Meng Yang <yobank@gmail.com>
2020-04-12 14:28:45 -0700

    added a button to open a new activity

--- app/src/main/java/edu/tacoma/wa/githubtest/MainActivity.java
+++ app/src/main/java/edu/tacoma/wa/githubtest/MainActivity.java
@@ -2,13 +2,20 @@ package edu.tacoma.wa.githubtest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-// Main Activity
+import android.view.View;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openActivity2(View view) {
+        Intent intent = new Intent(this, Activity2.class);
+        startActivity(intent);
     }
 }
